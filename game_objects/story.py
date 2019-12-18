763c1ae055edcae947366ab04c97ddf43c8f3c8d
Martin Kristensen <MKRI@JOHANNESFOG.DK>
2019-12-18 10:05:49 +0100

    ny med rigtigt navn

--- game_objects/story.py
+++ game_objects/story.py
@@ -47,7 +47,7 @@ class Level0:
 class Level1:
   def __init__(self):
     # Load resources
-    music               = common.load_music('Test1')
+    music               = common.load_music('Test1.ogg')
     next_level_sound    = False
 
     background_sprite   = animation.Animate('sky2.jpg',( 0,0, setting.screen_width, setting.screen_height))
